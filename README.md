08a18df6045e66f6815f8f2ff18f881380386d11
nkazakova <natalia.kazakova@innoway.ru>
2019-08-05 20:51:03 +0300

    IPhone81.png

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather-Mappy
+
+An application that displays weather on a map in 10 locations near the user's location
+
+## Screenshots
+
+![IPhone8](https://github.com/kazakovaNetIOS/Weather-Mappy/blob/master/screenshots/IPhone81.png)
+![IPhone8](https://github.com/kazakovaNetIOS/Weather-Mappy/blob/master/screenshots/IPhone82.png)
--- Weather Mappy/AppDelegate.swift
+++ Weather Mappy/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyBNIPRWqoj7egGk57OsHovnszRzEvDqiUQ")
+        GMSServices.provideAPIKey("***")
         return true
     }
 }
